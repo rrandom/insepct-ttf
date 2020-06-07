383051f9372b73cd3b1463edda8a415d2c18a081
rrandom <emanonhere@gmail.com>
2020-06-07 14:40:50 +0800

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
 use std::path::PathBuf;
-use iced::{
-    button, Align, Button, Element, Text
-};
+use iced::{executor, Application, Command, Element, Settings, Text, Row, Length,HorizontalAlignment};
 
+pub fn main() {
+    App::run(Settings::default())
+}
+
+#[derive(Debug)]
+enum Message {
+    OpenFilePressed
+}
 
 struct App {
     font_path: Option<PathBuf>,
 }
 
-fn main() {
-    println!("hello");
+impl Application for App {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (App, Command<Self::Message>) {
+        (App { font_path: None }, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("TTF-Inspector")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        let project_label = Text::new("Font Path: ")
+        .width(Length::Shrink)
+        .size(32)
+        .color([0.5, 0.5, 0.5])
+        .horizontal_alignment(HorizontalAlignment::Left);
+
+        project_label.into()
+    }
+
 }
