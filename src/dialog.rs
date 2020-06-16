63dc7041fcb1bcadb2f5f16151e255052ffc0998
rrandom <emanonhere@gmail.com>
2020-06-16 22:41:03 +0800

    load font

--- src/dialog.rs
+++ src/dialog.rs
@@ -2,11 +2,18 @@ use nfd::Response;
 use std::io;
 use std::path::{Path, PathBuf};
 
+#[derive(Clone, Debug)]
+pub struct RawFontInfo{
+    pub path: PathBuf,
+    pub data: Vec<u8>
+}
+
 pub async fn open_dialog() -> Result<PathBuf, io::Error> {
     let result: nfd::Response =
         match async { return nfd::open_file_dialog(Some("ttf"), None) }.await {
             Ok(result) => result,
             Err(e) => {
+                dbg!(&e);
                 return Err(io::Error::new(
                     io::ErrorKind::InvalidData,
                     "Unable to unwrap data from new file dialog",
@@ -43,7 +50,7 @@ pub async fn open_dialog() -> Result<PathBuf, io::Error> {
     }
 }
 
-pub async fn open() -> Result<PathBuf, super::LoadError> {
+pub async fn open() -> Result<RawFontInfo, super::LoadError> {
     use super::LoadError;
 
     let path = match open_dialog().await {
@@ -54,5 +61,10 @@ pub async fn open() -> Result<PathBuf, super::LoadError> {
         }
     };
 
-    Ok(path)
+    let font_data = async_std::fs::read(path.as_path()).await.unwrap();
+
+    Ok(RawFontInfo{
+        path,
+        data: font_data
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use iced::{
-    button, executor, Application, Button, Command, Container, Element, HorizontalAlignment,
+    button, executor, Application, Button, Command, Element, HorizontalAlignment,
     Length, Row, Settings, Text,
 };
 use std::path::PathBuf;
 
+mod dialog;
+
+use dialog::RawFontInfo;
+
 pub fn main() {
     App::run(Settings::default())
 }
-
-mod dialog;
 
 #[derive(Debug, Clone)]
 pub enum LoadError {
@@ -17,14 +19,33 @@ pub enum LoadError {
 }
 
 #[derive(Debug, Clone)]
+pub struct FontInfo {
+    family_name: String,
+    number_of_glyphs: u16
+}
+
+#[derive(Debug, Clone)]
 enum Message {
     OpenFilePressed,
-    Loaded(Result<PathBuf, LoadError>),
+    Loaded(Result<RawFontInfo, LoadError>),
+    Parsed(Option<FontInfo>),
+}
+
+
+pub async fn parse_font(data: Vec<u8>) -> Option<FontInfo> {
+    match async { return ttf_parser::Font::from_data(&data[..], 0) }.await {
+        Some(f) => Some(FontInfo {
+            family_name: f.family_name().unwrap(),
+            number_of_glyphs: f.number_of_glyphs(),
+        }),
+        None => None,
+    }
 }
 
 struct App {
     font_path: Option<PathBuf>,
     button_state: button::State,
+    font_info: Option<FontInfo>,
 }
 
 impl Application for App {
@@ -36,6 +57,7 @@ impl Application for App {
         let app = App {
             font_path: None,
             button_state: button::State::new(),
+            font_info: None,
         };
         (app, Command::none())
     }
@@ -49,9 +71,21 @@ impl Application for App {
             Message::OpenFilePressed => return Command::perform(dialog::open(), Message::Loaded),
             Message::Loaded(r) => {
                 dbg!(&r);
-                if let Some(v) = r.ok().take() {
-                    self.font_path = Some(v);
+                if let Some(RawFontInfo{ path, data}) = r.ok().take() {
+                    self.font_path = Some(path);
+                    return Command::perform(parse_font(data), Message::Parsed);
                 }
+            },
+            Message::Parsed(f) => {
+                let font = match f {
+                    Some(f) => f,
+                    None => {
+                        eprint!("Error: failed to open a font.");
+                        std::process::exit(1);
+                    },
+                };
+                dbg!(&font);
+                self.font_info = Some(font);
             }
         }
 
@@ -71,7 +105,12 @@ impl Application for App {
 
         let path = self.font_path.as_ref().and_then(|v| v.to_str()).unwrap_or("");
 
-        let row = Row::new().spacing(20).push(open_btn).push(project_label).push(Text::new(path));
+        let font_name = self.font_info.as_ref().and_then(|v| Some(v.family_name.as_str())).unwrap_or("");
+
+        let row = Row::new().spacing(20).push(open_btn).push(project_label)
+            .push(Text::new(path))
+            .push(Text::new(font_name));
+
 
         row.into()
     }
