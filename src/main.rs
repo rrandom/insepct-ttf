a72b349432193bc405e9ec9eb69c4dafcba1a1b4
rrandom <emanonhere@gmail.com>
2020-08-14 21:21:30 +0800

    render glyph wrong

--- src/main.rs
+++ src/main.rs
@@ -325,28 +325,50 @@ mod glyph_canvas {
                     .units_per_em()
                     .ok_or("invalid units per em")
                     .unwrap();
+
+                let (glyph_scale, glyph_baseline, glyph_size) = self.calc_scale(&bounds, &bbox);
+
+                let get_ypx = |yunits: f32| glyph_baseline - yunits * glyph_scale;
+
                 let scale = FONT_SIZE / units_per_em as f32;
-
-                // self.calc_scale(&bounds, &bbox);
-
                 let glyph = self.state.cache.draw(bounds.size(), |frame| {
+                    let iced::Size { width, height } = bounds.size();
+                    frame.fill_rectangle(
+                        Point::ORIGIN,
+                        bounds.size(),
+                        Color::from_rgba(0.1, 0.1, 0.1, 0.1),
+                    );
+                    frame.fill_rectangle(
+                        Point::new(width / 2.0, 0.0),
+                        iced::Size::new(1.0, height),
+                        Color::BLACK,
+                    );
+
+                    frame.fill_rectangle(
+                        Point::new(0.0, height / 2.0),
+                        iced::Size::new(width, 1.0),
+                        Color::BLACK,
+                    );
+
                     let path = self.get_path();
                     let cell_size = bounds.height.min(bounds.width);
-
                     let bbox_w = (bbox.x_max - bbox.x_min) as f32 * scale;
                     let dx = (cell_size - bbox_w) / 2.0;
                     let y = cell_size + self.font.descender() as f32 * scale;
 
+                    dbg!(self.font.ascender(), self.font.descender());
+
+                    // frame.translate(Vector::new(-width / 2.0, -height / 2.0));
+                    // frame.scale(glyph_scale, glyph_scale);
+
                     frame.translate(Vector::new(dx as f32, y as f32));
                     frame.scale(scale, scale);
                     frame.scale(1.0, -1.0);
+                    frame.fill(&path, Color::BLACK);
 
-                    frame.fill(&path, Color::from_rgb8(0x0, 0x0, 0x0));
-
-                    frame.fill(
-                        &Path::rectangle(Point::ORIGIN, bounds.size()),
-                        Color::from_rgba(0.5, 0.5, 0.5, 0.5),
-                    );
+                    self.h_line("Baseline", 0.0, frame);
+                    self.h_line("Ascender", self.font.ascender() as f32, frame);
+                    self.h_line("Descender", self.font.descender() as f32, frame);
                 });
                 return vec![glyph];
             }
@@ -387,7 +409,22 @@ mod glyph_canvas {
             })
         }
 
-        fn calc_scale(&self, bounds: &Rectangle, bbox: &owned_ttf_parser::Rect) {
+        fn h_line(&self, content: &str, y: f32, frame: &mut iced::canvas::Frame) {
+            let text = iced::canvas::Text {
+                content: content.into(),
+                position: iced::Point { x: 2.0, y: y + 3.0 },
+                ..Default::default()
+            };
+
+            frame.fill_text(text);
+            frame.fill_rectangle(
+                iced::Point::new(80.0, y),
+                iced::Size::new(1000.0, 2.0),
+                Color::from_rgb(0.8, 0.0, 0.0),
+            );
+        }
+
+        fn calc_scale(&self, bounds: &Rectangle, bbox: &owned_ttf_parser::Rect) -> (f32, f32, f32) {
             let glyph_width = bounds.width - GLYPH_MARGIN * 2.0;
             let glyph_height = bounds.height - GLYPH_MARGIN * 2.0;
 
@@ -395,10 +432,20 @@ mod glyph_canvas {
                 / ((bbox.x_max - bbox.x_min) as f32)
                     .min(glyph_height / (bbox.y_max - bbox.y_min) as f32);
 
+            let units_per_em = self
+                .font
+                .units_per_em()
+                .ok_or("invalid units per em")
+                .unwrap() as f32;
+
+            let glyph_size = glyph_scale * units_per_em;
+
             let glyph_baseline =
                 GLYPH_MARGIN + glyph_height * (bbox.y_max / (bbox.y_max - bbox.y_min)) as f32;
 
             dbg!(&glyph_scale, &glyph_baseline);
+
+            (glyph_scale, glyph_baseline, glyph_size)
         }
     }
 }
@@ -491,8 +538,6 @@ mod glyph_info {
                     .push(iced::Text::new(format!("xMax {}", &rect.x_max)))
                     .push(iced::Text::new(format!("yMin {}", &rect.y_min)))
                     .push(iced::Text::new(format!("yMax {}", &rect.y_max)));
-
-                // self.outline.as_ref().map(|f| dbg!(f));
             }
 
             c.into()
